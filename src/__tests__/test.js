b52d458e82df9f1cab6c487912c79fd7cf5c8c4d
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-06-21 09:55:03 +0200

    feat: initial implementation (#1)
    * feat: initial implementation
    
    * test: add base test
    
    * fix: increase iteration and predict on x
    
    * docs: rename variables and add source

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,7 +1,26 @@
-import myModule from '..';
+import baselineCorrection from '..';
 
-describe('test myModule', () => {
-    it('should return 42', () => {
-        expect(myModule()).toEqual(42);
+// https://en.wikipedia.org/wiki/Cauchy_distribution
+function lorentzian(x, x0 = 0, gamma = 1) {
+    return (gamma * gamma) / (Math.PI * gamma * (gamma * gamma + (x - x0) * (x - x0)));
+}
+
+describe('Simulated', () => {
+    it('Lineal baseline', () => {
+        const size = 30;
+        const fourth = size / 4;
+        let times = new Array(size);
+        let original = new Array(size);
+        let noisy = new Array(size);
+        for (let i = 0; i < size; ++i) {
+            times[i] = i;
+            original[i] = 8 * lorentzian(i, fourth) + 16 * lorentzian(i, 2 * fourth) + 8 * lorentzian(i, 3 * fourth);
+            noisy[i] = original[i] + i;
+        }
+
+        let {delta, iteration} = baselineCorrection(times, noisy);
+
+        expect(delta).toBeLessThan(0.001);
+        expect(iteration).toBeLessThan(100);
     });
 });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,61 @@
+import PolynomialRegression from 'ml-regression-polynomial';
+
 /**
- * Returns a very important number
- * @return {number}
+ * Iterative regression-based baseline correction
+ * @param {Array<number>} x - Independent axis variable
+ * @param {Array<number>} y - Dependent axis variable
+ * @param {object} [options] - Options object
+ * @param {number} [options.maxIterations = 100] - Maximum number of allowed iterations
+ * @param {function} [options.Regression = PolynomialRegression] - Regression class with a predict method
+ * @param {*} [options.regressionOptions] - Options for regressionFunction
+ * @param {number} [options.tolerance = 0.001] - Convergence error tolerance
+ * @return {{corrected: Array<number>, delta: number, iteration: number, baseline: Array<number>}}
  */
-export default function myModule() {
-    return 42;
+export default function (x, y, options = {}) {
+    let {
+        maxIterations = 100,
+        Regression = PolynomialRegression,
+        regressionOptions,
+        tolerance = 0.001
+    } = options;
+
+    if (!regressionOptions && Regression === PolynomialRegression) {
+        regressionOptions = 3;
+    }
+
+    let baseline = y.slice();
+    let fitting = y.slice();
+    let oldFitting = y;
+    let iteration = 0;
+    let delta;
+    while (iteration < maxIterations) {
+        // Calculate the fitting result
+        let regression = new Regression(x, baseline, regressionOptions);
+
+        delta = 0;
+        for (var i = 0; i < baseline.length; i++) {
+            fitting[i] = regression.predict(x[i]);
+            if (baseline[i] > fitting[i]) {
+                baseline[i] = fitting[i];
+            }
+
+            delta += Math.abs((fitting[i] - oldFitting[i]) / oldFitting[i]);
+        }
+
+        // Stop criterion
+        if (delta < tolerance) {
+            break;
+        } else {
+            oldFitting = fitting.slice();
+            iteration++;
+        }
+    }
+
+    // removes baseline
+    let corrected = new Array(baseline.length);
+    for (var j = 0; j < baseline.length; j++) {
+        corrected[j] = y[j] - baseline[j];
+    }
+
+    return {corrected, delta, iteration, baseline};
 }
