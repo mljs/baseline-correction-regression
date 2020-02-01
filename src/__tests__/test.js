9947c94f79811d1864cb1efc1403cf892645b698
jobo322 <jose.bolanos@correounivalle.edu.co>
2020-02-01 09:17:18 -0500

    reduce nbPoints to fit baseline and adding prettier format

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -2,25 +2,30 @@ import baselineCorrection from '..';
 
 // https://en.wikipedia.org/wiki/Cauchy_distribution
 function lorentzian(x, x0 = 0, gamma = 1) {
-    return (gamma * gamma) / (Math.PI * gamma * (gamma * gamma + (x - x0) * (x - x0)));
+  return (
+    (gamma * gamma) / (Math.PI * gamma * (gamma * gamma + (x - x0) * (x - x0)))
+  );
 }
 
 describe('Simulated', () => {
-    it('Lineal baseline', () => {
-        const size = 30;
-        const fourth = size / 4;
-        let times = new Array(size);
-        let original = new Array(size);
-        let noisy = new Array(size);
-        for (let i = 0; i < size; ++i) {
-            times[i] = i;
-            original[i] = 8 * lorentzian(i, fourth) + 16 * lorentzian(i, 2 * fourth) + 8 * lorentzian(i, 3 * fourth);
-            noisy[i] = original[i] + i;
-        }
+  it('Lineal baseline', () => {
+    const size = 30;
+    const fourth = size / 4;
+    let times = new Array(size);
+    let original = new Array(size);
+    let noisy = new Array(size);
+    for (let i = 0; i < size; ++i) {
+      times[i] = i;
+      original[i] =
+        8 * lorentzian(i, fourth) +
+        16 * lorentzian(i, 2 * fourth) +
+        8 * lorentzian(i, 3 * fourth);
+      noisy[i] = original[i] + i;
+    }
 
-        let {delta, iteration} = baselineCorrection(times, noisy);
+    let { delta, iteration } = baselineCorrection(times, noisy);
 
-        expect(delta).toBeLessThan(0.001);
-        expect(iteration).toBeLessThan(100);
-    });
+    expect(delta).toBeLessThan(0.001);
+    expect(iteration).toBeLessThan(100);
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import PolynomialRegression from 'ml-regression-polynomial';
+import { XY } from 'ml-spectra-processing';
 
 /**
  * Iterative regression-based baseline correction
@@ -11,51 +12,64 @@ import PolynomialRegression from 'ml-regression-polynomial';
  * @param {number} [options.tolerance = 0.001] - Convergence error tolerance
  * @return {{corrected: Array<number>, delta: number, iteration: number, baseline: Array<number>}}
  */
-export default function (x, y, options = {}) {
-    let {
-        maxIterations = 100,
-        Regression = PolynomialRegression,
-        regressionOptions,
-        tolerance = 0.001
-    } = options;
+const baselineCorrection = function(x, y, options = {}) {
+  let {
+    maxIterations = 100,
+    Regression = PolynomialRegression,
+    regressionOptions,
+    tolerance = 0.001,
+    reduce = true,
+    nbPoints = 4001,
+  } = options;
 
-    if (!regressionOptions && Regression === PolynomialRegression) {
-        regressionOptions = 3;
+  if (!regressionOptions && Regression === PolynomialRegression) {
+    regressionOptions = 3;
+  }
+
+  let xTemp, yTemp;
+  if (reduce) {
+    let reducedData = XY.reduce(x, y, { nbPoints });
+    yTemp = reducedData.y;
+    xTemp = reducedData.x;
+  } else {
+    xTemp = x.slice();
+    yTemp = y.slice();
+  }
+
+  let baseline = yTemp.slice();
+  let fitting = yTemp.slice();
+  let oldFitting = yTemp.slice();
+  let iteration = 0;
+  let delta, regression;
+  while (iteration < maxIterations) {
+    // Calculate the fitting result
+    regression = new Regression(xTemp, baseline, regressionOptions);
+
+    delta = 0;
+    for (let i = 0; i < baseline.length; i++) {
+      fitting[i] = regression.predict(xTemp[i]);
+      if (baseline[i] > fitting[i]) {
+        baseline[i] = fitting[i];
+      }
+
+      delta += Math.abs((fitting[i] - oldFitting[i]) / oldFitting[i]);
     }
 
-    let baseline = y.slice();
-    let fitting = y.slice();
-    let oldFitting = y;
-    let iteration = 0;
-    let delta;
-    while (iteration < maxIterations) {
-        // Calculate the fitting result
-        let regression = new Regression(x, baseline, regressionOptions);
-
-        delta = 0;
-        for (var i = 0; i < baseline.length; i++) {
-            fitting[i] = regression.predict(x[i]);
-            if (baseline[i] > fitting[i]) {
-                baseline[i] = fitting[i];
-            }
-
-            delta += Math.abs((fitting[i] - oldFitting[i]) / oldFitting[i]);
-        }
-
-        // Stop criterion
-        if (delta < tolerance) {
-            break;
-        } else {
-            oldFitting = fitting.slice();
-            iteration++;
-        }
+    // Stop criterion
+    if (delta < tolerance) {
+      break;
+    } else {
+      oldFitting = fitting.slice();
+      iteration++;
     }
+  }
 
-    // removes baseline
-    let corrected = new Array(baseline.length);
-    for (var j = 0; j < baseline.length; j++) {
-        corrected[j] = y[j] - baseline[j];
-    }
+  // removes baseline
+  if (reduce) baseline = x.map((e) => regression.predict(e));
 
-    return {corrected, delta, iteration, baseline};
-}
+  let corrected = baseline.map((e, i) => y[i] - e);
+
+  return { corrected, delta, iteration, baseline };
+};
+
+export { baselineCorrection as default };
