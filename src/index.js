e1142035ac849ebc61231d0905934fe2141c829d
Luc Patiny <luc@patiny.com>
2020-02-04 16:24:09 +0100

    chore: update dependencies

--- src/index.js
+++ src/index.js
@@ -11,51 +11,51 @@ import PolynomialRegression from 'ml-regression-polynomial';
  * @param {number} [options.tolerance = 0.001] - Convergence error tolerance
  * @return {{corrected: Array<number>, delta: number, iteration: number, baseline: Array<number>}}
  */
-export default function (x, y, options = {}) {
-    let {
-        maxIterations = 100,
-        Regression = PolynomialRegression,
-        regressionOptions,
-        tolerance = 0.001
-    } = options;
+export default function baselineCorrectionRegression(x, y, options = {}) {
+  let {
+    maxIterations = 100,
+    Regression = PolynomialRegression,
+    regressionOptions,
+    tolerance = 0.001,
+  } = options;
 
-    if (!regressionOptions && Regression === PolynomialRegression) {
-        regressionOptions = 3;
+  if (!regressionOptions && Regression === PolynomialRegression) {
+    regressionOptions = 3;
+  }
+
+  let baseline = y.slice();
+  let fitting = y.slice();
+  let oldFitting = y;
+  let iteration = 0;
+  let delta;
+  while (iteration < maxIterations) {
+    // Calculate the fitting result
+    let regression = new Regression(x, baseline, regressionOptions);
+
+    delta = 0;
+    for (let i = 0; i < baseline.length; i++) {
+      fitting[i] = regression.predict(x[i]);
+      if (baseline[i] > fitting[i]) {
+        baseline[i] = fitting[i];
+      }
+
+      delta += Math.abs((fitting[i] - oldFitting[i]) / oldFitting[i]);
     }
 
-    let baseline = y.slice();
-    let fitting = y.slice();
-    let oldFitting = y;
-    let iteration = 0;
-    let delta;
-    while (iteration < maxIterations) {
-        // Calculate the fitting result
-        let regression = new Regression(x, baseline, regressionOptions);
-
-        delta = 0;
-        for (var i = 0; i < baseline.length; i++) {
-            fitting[i] = regression.predict(x[i]);
-            if (baseline[i] > fitting[i]) {
-                baseline[i] = fitting[i];
-            }
-
-            delta += Math.abs((fitting[i] - oldFitting[i]) / oldFitting[i]);
-        }
-
-        // Stop criterion
-        if (delta < tolerance) {
-            break;
-        } else {
-            oldFitting = fitting.slice();
-            iteration++;
-        }
+    // Stop criterion
+    if (delta < tolerance) {
+      break;
+    } else {
+      oldFitting = fitting.slice();
+      iteration++;
     }
+  }
 
-    // removes baseline
-    let corrected = new Array(baseline.length);
-    for (var j = 0; j < baseline.length; j++) {
-        corrected[j] = y[j] - baseline[j];
-    }
+  // removes baseline
+  let corrected = new Array(baseline.length);
+  for (let j = 0; j < baseline.length; j++) {
+    corrected[j] = y[j] - baseline[j];
+  }
 
-    return {corrected, delta, iteration, baseline};
+  return { corrected, delta, iteration, baseline };
 }
